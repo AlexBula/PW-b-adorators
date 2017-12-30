0759e33ebef4312ee598853280bf7bed87a49214
Alex Bula <bula.aleksander@gmail.com>
2017-12-30 16:18:34 +0100

    Wstępna optymalizacja

--- adorate-42.cpp
+++ adorate-42.cpp
@@ -11,34 +11,36 @@
 #include <memory>
 #include <deque>
 #include <thread>
+#include <chrono>
 
 
 
 const std::string REGEX = "([0-9]+) ([0-9]+) ([0-9]+)";
+static std::regex graf(REGEX);
 
-std::vector<int> convert;
-std::map<int,int> new_numbers;
+static std::vector<int> convert;
+static std::map<int,int> new_numbers;
 
 std::vector<int> b;
-// std::deque<std::atomic<int>> b;
+
 std::deque<std::atomic<int>> db;
 
-std::vector<int> V;
-std::vector<int> R;
+static std::vector<int> V;
+static std::vector<int> Q;
+thread_local std::vector<int> R;
 
 // Mapa krawedz -> waga
-std::map<std::pair<int,int>,int> W; 
+static std::map<std::pair<int,int>,int> W; 
 
 // Set wszystkich wierzchołkow
-std::set<int> NODES;
+static std::set<int> NODES;
 
 
-int numer = 0;
+int number = 0;
 
 struct paircmp {
-    bool operator() (std::pair<int,int> const & lhs, std::pair<int,int> const & rhs) const
-    {
-    return (lhs.second > rhs.second) || (lhs.second == rhs.second && convert[lhs.first] > convert[rhs.first]);
+    inline bool operator() (std::pair<int,int> const & lhs, std::pair<int,int> const & rhs) const {
+        return (lhs.second > rhs.second) || (lhs.second == rhs.second && convert[lhs.first] > convert[rhs.first]);
     }
 };
 
@@ -46,9 +48,7 @@ std::vector<std::set<std::pair<int,int>,paircmp>> N;
 std::vector<std::set<std::pair<int,int>,paircmp>> S;
 
 
-static std::regex graf(REGEX);
 
-std::atomic_flag argmax_lock = ATOMIC_FLAG_INIT;
 
 std::vector<std::unique_ptr<std::mutex>> SMutexes;
 
@@ -69,48 +69,36 @@ void wypisz_adorowanych() {
 
 
 void update_bvalue(int b_method) {
-
-    // b.clear();
     for (auto i : NODES) {
-        // b.emplace_back(bvalue(b_method, i));
         b[new_numbers[i]] = bvalue(b_method, i);
     }
 }
 
 void zero_db() {
-    db.clear();
-    for (auto v : NODES) {
-        db.emplace_back(0);
-    }
+    std::fill(db.begin(), db.end(), 0);
 }
 
-
-void clear_S() {
-    for (auto it = S.begin(); it != S.end(); ++it) {
-        it->clear();
+void copy_db_to_b() {
+    for (int i = 0; i < db.size(); ++i) {
+        b[i] = db[i];
     }
 }
 
 // Argmax (zwraca -1 zamiast nulla)
 int argmax(int u, int b_method) {
 
-    struct paircmp compare;
+    static struct paircmp compare;
 
     // Przechodze po wszystkich wezlach az znajde taki ktory mi pasuje
     for (auto it = N[u].begin(); it != N[u].end(); ++it) {
         
         int v = it->first;
-        auto p = std::make_pair(u, W[{u,v}]);
-        
-
         if (S[v].find({u,W[{v,u}]}) != S[v].end()) continue;
         if (S[v].size() < bvalue((uint)b_method, (ulong)convert[v])) {
             return v;
         }
         else if (bvalue(b_method, (ulong)convert[v]) > 0) {
-            std::pair<int,int> p2 = *(--S[v].end());
-            // p2.first = convertp2.first;
-            if (compare(p, p2)) return v;
+            if (compare({u, W[{u,v}]}, *(--S[v].end()))) return v;
         }
     }
     return -1;
@@ -147,14 +135,14 @@ void get_adorators(int u, int b_method, int id) {
                 // log("get_adorators: watek ", id, ", wybral wezel ", convert[x]);
                 S[x].insert({u,W[{x,u}]});
                 if (y != -1) {
-                    RMutex.lock();
                     S[x].erase(--S[x].end());
                     // log("get_adorators: watek ", id,  ", zwieksza wezel  ", convert[y]);
                     if (db[y] == 0) {
+                        // RMutex.lock();
                         R.push_back(y);
+                        // RMutex.unlock();
                     }
                     db[y]++;
-                    RMutex.unlock();
                 }
                 i++;
             }
@@ -165,33 +153,35 @@ void get_adorators(int u, int b_method, int id) {
 }
 
 void add_new_node(int v) {
-    new_numbers[v] = numer;
+    new_numbers[v] = number;
     b.push_back(0);
     db.emplace_back(0);   
     N.push_back(std::set<std::pair<int,int>,paircmp>());
-    S.push_back(std::set<std::pair<int,int>,paircmp>());
+    // S.push_back(std::set<std::pair<int,int>,paircmp>());
     convert.push_back(v);
     NODES.insert(v);
     SMutexes.push_back(std::make_unique<std::mutex>());
-    numer++;
+    V.push_back(number);
+    number++;
 }
 
 
-void copy_db_to_b() {
-    for (int i = 0; i < db.size(); ++i) {
-        b[i] = db[i];
-    }
-}
 
-void find_adorators(std::vector<int> C, int b_method, int id) {
+void find_adorators(std::vector<int> &C, int b_method, int id) {
 
     for (auto it = C.begin(); it != C.end(); ++it) {
         get_adorators(*it, b_method, id);
     }
+    RMutex.lock();
+    Q.insert(Q.begin(), R.begin(), R.end());
+    RMutex.unlock();
 }
 
 int main(int argc, char* argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::ios_base::sync_with_stdio(false);
+
 
     if (argc != 4) {
         std::cerr << "usage: "<<argv[0]<<" thread-count inputfile b-limit"<< std::endl;
@@ -231,16 +221,16 @@ int main(int argc, char* argv[]) {
     } else {
         std::cerr << "Error: couldn't open file: " << input_filename << '\n';
     } 
+    S = std::vector<std::set<std::pair<int,int>,paircmp>>(number);
+    std::fill(S.begin(), S.end(), std::set<std::pair<int,int>,paircmp>());
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "Graf wczytany po " << elapsed.count() << " sekundach" << std::endl;
 
 
-    int l = 0;
-    for (auto i : convert) {
-        V.push_back(l);
-        l++;
-    }
 
 
-    std::vector<int> Q;
     for (int b_method = 0; b_method < b_limit + 1  ; b_method++) {
         Q = V;
         update_bvalue(b_method);
@@ -254,36 +244,29 @@ int main(int argc, char* argv[]) {
                 C[i % thread_count].push_back(Q[i]);
             }
 
+            Q.clear();
             // Odpalanie puli watkow;
             for (auto i = 0; i < thread_count; ++i) {
-                threads[i] = std::thread(find_adorators, C[i], b_method, i);
+                threads[i] = std::thread(find_adorators, std::ref(C[i]), b_method, i);
             }
 
             for (auto i = 0; i < thread_count; ++i) {
                 threads[i].join();
             }
 
-            Q = R;
             R.clear();
             copy_db_to_b();
             zero_db();
         }
 
-
-        // wypisz_adorowanych();
         long long sum = 0;
         
         for (auto it = 0; it < S.size(); ++it) {
-            std::set<std::pair<int,int>, paircmp> se = S[it];
-            for (auto its = se.begin(); its != se.end(); ++its) {
-                // std::cout << "Wezeł " << convert[it] << " jest adorowany przez " << convert[its->first] << '\n';
+            for (auto its = S[it].begin(); its != S[it].end(); ++its) {
                 sum += its->second;
             }
+            S[it].clear();
         }
         std::cout << sum/2 << std::endl;
-        clear_S();
     }
-    
-
-
 }
